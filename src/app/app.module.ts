13b2bce8bbcd8473ceb6d74d5dc1079ed6710334
Shubhangi <shubhangi.thakur@neosofttech.com>
2018-04-03 10:31:03 +0530

    map component and slider created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SlickModule } from 'ngx-slick';
+// import { CarouselModule } from 'angular4-carousel';
 
 
 import { AppComponent } from './app.component';
 import { WelcomescreenComponent } from './welcomescreen/welcomescreen.component';
+import { MapComponent } from './map/map.component';
+import { CountrysliderComponent } from './countryslider/countryslider.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomescreenComponent
+    WelcomescreenComponent,
+    MapComponent,
+    CountrysliderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SlickModule.forRoot()
+    // CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/countryslider/countryslider.component.ts
@@ -0,0 +1,31 @@
+import { Component,OnInit } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {SlickModule} from 'ngx-slick';
+
+
+
+@Component({
+  selector: 'app-countryslider',
+  templateUrl: './countryslider.component.html',
+  styleUrls: ['./countryslider.component.scss']
+})
+export class CountrysliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+  slides = [
+      {img: 'assets/Images/UAEImages/16650.jpg'},
+      {img: 'assets/Images/UAEImages/36345205-dubai-pics.jpg'},
+      {img: 'assets/Images/UAEImages/d1b2337a309c5e6349b9f67a47abda29.jpg'},
+      {img: 'assets/Images/UAEImages/Downtown-Dubai.jpg'},
+      {img: 'assets/Images/UAEImages/family-days-out-dubai-800x444.jpg'},
+      {img: 'assets/Images/UAEImages/Ferris-wheel-in-Al-Qasba-Sharjah.jpg'}
+    ];
+    slideConfig = {'slidesToShow': 1, 'slidesToScroll': 1, 'autoplay':true ,'autospeed':1000};
+
+
+
+}
