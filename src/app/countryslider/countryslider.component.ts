7c5157c2a7cfd7e3d6b60a56cad316f34ac0f171
Shubhangi <shubhangi.thakur@neosofttech.com>
2018-04-05 13:56:48 +0530

    slider changes

--- src/app/countryslider/countryslider.component.ts
+++ src/app/countryslider/countryslider.component.ts
@@ -8,18 +8,16 @@ import { SlickModule } from 'ngx-slick';
   styleUrls: ['./countryslider.component.scss']
 })
 export class CountrysliderComponent implements OnInit {
-
+   slides:Array<Object>;
+   slideConfig;
   constructor() { }
-
   ngOnInit() {
+    this.slides = [
+        { img: 'assets/Images/KSA/1.png'},
+        { img: 'assets/Images/KSA/2.png'}
+      ];
+   this.slideConfig = {'slidesToShow': 1, 'slidesToScroll': 1, 'autoplay':true , 'autoplaySpeed': 7000,'speed':3000 };
 
   }
-  slides = [
-      {img: 'assets/Images/KSA/1.png'},
-      {img: 'assets/Images/KSA/2.png'}
-      ];
-    slideConfig = {'slidesToShow': 1, 'slidesToScroll': 1, 'autoplay':true ,'speed':8000};
-
-
 
 }
