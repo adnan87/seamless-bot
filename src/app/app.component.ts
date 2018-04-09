516b53cf7695477fe0bc70f5287300f5396c2926
Shubhangi <shubhangi.thakur@neosofttech.com>
2018-04-09 11:45:15 +0530

    changes in welcome screen amd slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Shift-bot';
-
-  countryChange(event){
-    console.log(event)
+  public edited = true;
+  ngOnInit(){
+    this.hideSlider()
   }
+  hideSlider(): void {
+    let temp = this;
+     setTimeout(function() {
+         temp.edited = false;
+     }, 10000);
+    }
 }
